1d36a3de9a8962eb05041f28f8988e00c1c2acc0
Shubham Raj <shubhamaryan805@gmail.com>
2022-11-24 23:25:36 +0530

    Added: Logic to create simple linkedlist

---
+++ src/linkedlists/Node.java
@@ -0,0 +1,14 @@
+package linkedlists;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    /*
+     * Linked list node
+     */
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
